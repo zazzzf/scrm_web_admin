5b580a6ef7cc54871aee2df45805ce2b435f968a
Zaz_zzf <40158049+zazzzf@users.noreply.github.com>
2020-04-08 18:22:06 +0800

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -12,10 +12,10 @@ let debug =  true;//是否测试模式
 // console.log(process.env.NODE_ENV)
 if (debug) {
     //测试
-    apiUrl = 'http://scs-api-dev.xaxhl.net/index.php?s=';
+    apiUrl = '';
 }else{
     //线上
-    apiUrl = 'http://scrm-api.xaxhl.net/index.php?s=';
+    apiUrl = '';
 }
 config.API_URL = apiUrl;
 
@@ -23,11 +23,11 @@ config.ADMIN_LIST = ["xhl","koko","houlianjing","heci","nick","admin","diandian"
 /**
  * 七牛云
  */
-config.QINIU_BASE_URL = "http://up-z1.qiniup.com"                           //七牛上传地址
-config.QINIU_URL_TYPE_IMG = "http://scrm-image-qn.xaxhl.net/"               //七牛 图片地址
-config.QINIU_URL_TYPE_VIDEO = "http://scrm-video-qn.xaxhl.net/"             //七牛 视频地址
-config.QINIU_URL_TYPE_KNOWLEDGE = "http://scrm-knowledge-qn.xaxhl.net/"     //智库地址 为加密存储库
-config.QINIU_URL_TYPE_TIKTOK = "http://scrm-tiktok-video-qn.xaxhl.net/"     //抖音视频地址
-config.QINIU_URL_TYPE_REPORT = "http://scrm-report-qn.xaxhl.net/"           //报告地址
+config.QINIU_BASE_URL = ""                           //七牛上传地址
+config.QINIU_URL_TYPE_IMG = ""               //七牛 图片地址
+config.QINIU_URL_TYPE_VIDEO = ""             //七牛 视频地址
+config.QINIU_URL_TYPE_KNOWLEDGE = ""     //智库地址 为加密存储库
+config.QINIU_URL_TYPE_TIKTOK = ""     //抖音视频地址
+config.QINIU_URL_TYPE_REPORT = ""           //报告地址
 
 export default config
